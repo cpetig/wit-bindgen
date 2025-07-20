b16fe39c54148a8004ec7d2c4d9456c7e4248572
Christof Petig <christof.petig@arcor.de>
2025-07-20 16:24:52 +0200

    integrate changes from symmetric branch

--- crates/rust/src/bindgen.rs
+++ crates/rust/src/bindgen.rs
@@ -777,7 +777,7 @@ impl Bindgen for FunctionBindgen<'_, '_> {
                 results.push(len);
             }
 
-            Instruction::FixedSizeListLowerBlock {
+            Instruction::FixedSizeListLowerMemory {
                 element,
                 size: _,
                 id: _,

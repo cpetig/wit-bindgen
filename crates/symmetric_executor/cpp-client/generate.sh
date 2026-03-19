ff2a11c7805532bd5b1d1c8a9ca9e6ccc2771dbf
Christof Petig <christof.petig@arcor.de>
2026-03-19 01:03:18 +0100

    regenerate c++ import

--- crates/symmetric_executor/cpp-client/generate.sh
+++ crates/symmetric_executor/cpp-client/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ../../../target/debug/wit-bindgen cpp ../wit -w executor-import --symmetric --api-style symmetric --format
-../../../target/debug/wit-bindgen cpp ../wit -w module --symmetric --api-style symmetric --format --with symmetric:runtime/symmetric-executor@0.3.0=executor_import_cpp.h
+../../../target/debug/wit-bindgen cpp ../wit -w module --symmetric --api-style symmetric --format --with symmetric:runtime/symmetric-executor@0.3.1=executor_import_cpp.h
